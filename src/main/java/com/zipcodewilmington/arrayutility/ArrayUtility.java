47879a3f5dc2857f4c19db21ee30f6e51436621f
William Reyes <williamreyes814@yahoo.com>
2019-07-06 19:45:37 -0400

    getnumberofoccurrencs completed

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,36 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    private final T[] array;
+
+    public ArrayUtility(T[] array) {
+        this.array = array;
+    }
+
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return null;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        Integer numberOfOccurrences = 0;
+        for (int currentIndex = 0; currentIndex < this.array.length; currentIndex++) {
+            T currentValue = array[currentIndex];
+            if(valueToEvaluate == currentValue) {
+                numberOfOccurrences++;
+            }
+        }
+        return numberOfOccurrences;
+    }
+
+
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
 }
